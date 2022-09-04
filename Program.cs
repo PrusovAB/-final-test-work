e7d181c2d4e6245890f3a88403ff118bd2ba93c3
Prusov A V <105492820+PrusovAB@users.noreply.github.com>
2022-09-04 17:24:31 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] arrayStr = { "Hello", "2", "3", "Я", "В", "Шоке" };
+
+
+void PrintArray(string[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (i == 0) Console.Write("[");
+      if (i < array.Length - 1) Console.Write(array[i] + ",");
+      else Console.Write(array[i] + "]");
+   }
+}
+
+PrintArray(arrayStr);
+
+int count = 0;
+
+void SortArray(string[] array)
+{
+
+   for (int i = 0; i <= array.Length - 1; i++)
+   {
+      if (array[i].Length <= 3)
+      {
+         count++;
+      }
+   }
+   string[] arrNew = new string[count];
+   int cou = -1;
+   for (int i = 0; i < array.Length; i++)
+   {
+
+      cou++;
+      if (array[i].Length <= 3)
+      {
+         arrNew[i - cou] = array[i];
+         cou = 0;
+      }
+
+   }
+   Console.WriteLine();
+   PrintArray(arrNew);
+}
+
+SortArray(arrayStr);
